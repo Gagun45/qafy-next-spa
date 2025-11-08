57f823fbdb1349dafbda5fe50a82eb345dba8b65
Gagun45 <selyanchyn45@gmail.com>
2025-11-08 18:12:18 +0200

    container h-full applied

--- components/General/Container/Container.tsx
+++ components/General/Container/Container.tsx
@@ -6,6 +6,6 @@ interface Props {
 }
 
 const Container = ({ children, className }: Props) => {
-  return <div className={`max-w-6xl mx-auto ${className}`}>{children}</div>;
+  return <div className={`h-full w-full max-w-6xl mx-auto ${className}`}>{children}</div>;
 };
 export default Container;
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,8 +3,8 @@ import ThemeToggle from "../General/ThemeToggle/ThemeToggle";
 
 const Header = () => {
   return (
-    <header className="w-full">
-      <Container className="flex items-center gap-4">
+    <header className="w-full bg-accent h-24">
+      <Container className="flex items-center gap-4 px-4">
         <span>Qafy Mobile</span>
         <ThemeToggle />
       </Container>
