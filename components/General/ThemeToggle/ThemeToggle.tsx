24829681cd3318571a50710761421f6d67c5ece0
Gagun45 <selyanchyn45@gmail.com>
2025-11-08 19:00:00 +0200

    theme toggle designed

--- components/General/ThemeToggle/ThemeToggle.tsx
+++ components/General/ThemeToggle/ThemeToggle.tsx
@@ -13,10 +13,10 @@ const ThemeToggle = () => {
 
   return (
     <Button
-      variant="outline"
       size="icon"
       aria-label={"Theme toggle"}
       onClick={toggleTheme}
+      className="bg-foreground rounded-full text-background hover:bg-foreground"
     >
       <Moon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all! dark:-rotate-90 dark:scale-0" />
       <Sun className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all! dark:rotate-0 dark:scale-100" />
