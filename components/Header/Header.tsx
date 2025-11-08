1512a0640b72802add0630dd9001b3324777db50
Gagun45 <selyanchyn45@gmail.com>
2025-11-08 19:01:39 +0200

    header scrolled classname added

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,17 +1,23 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Container from "../General/Container/Container";
 import SmoothLink from "../General/SmoothLink/SmoothLink";
 import ThemeToggle from "../General/ThemeToggle/ThemeToggle";
 import { scrollSpy } from "react-scroll";
 
 const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
   useEffect(() => {
     scrollSpy.update();
+    const onScroll = () => {
+      setScrolled(window.scrollY > 400);
+    };
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
   }, []);
   return (
-    <header className="w-full bg-accent h-24 sticky top-0 z-10">
+    <header className={`${scrolled&&'outline outline-foreground'} w-full bg-accent h-24 sticky top-0 z-10`}>
       <Container className="flex items-center gap-4">
         <span>Qafy Mobile</span>
         <ThemeToggle />
