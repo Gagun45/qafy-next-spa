8e8e6fe962b99f19dc9b1446a66bfd3f39af0782
Gagun45 <selyanchyn45@gmail.com>
2025-11-12 12:06:56 +0200

    contact form title removed, name input messages added

--- components/Main/Contacts/RightCard/ContactForm/NameInput/NameInput.tsx
+++ components/Main/Contacts/RightCard/ContactForm/NameInput/NameInput.tsx
@@ -7,19 +7,23 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import type { ContactFormType } from "@/lib/zod-schemas";
+import { useTranslations } from "next-intl";
 import { useFormContext } from "react-hook-form";
 
 const NameInput = () => {
   const { control } = useFormContext<ContactFormType>();
+  const t = useTranslations("Contacts.FormFields.name");
+  const label = t("label");
+  const placeholder = t("placeholder");
   return (
     <FormField
       control={control}
       name="name"
       render={({ field }) => (
         <FormItem>
-          <FormLabel>Name</FormLabel>
+          <FormLabel>{label}</FormLabel>
           <FormControl>
-            <Input placeholder="John Doe" {...field} />
+            <Input placeholder={placeholder} {...field} />
           </FormControl>
           <FormMessage />
         </FormItem>
--- components/Main/Contacts/RightCard/RightCard.tsx
+++ components/Main/Contacts/RightCard/RightCard.tsx
@@ -3,7 +3,6 @@ import ContactForm from "./ContactForm/ContactForm";
 const RightCard = () => {
   return (
     <div className="w-full">
-      <h3 className="text-lg font-semibold">Contact Form</h3>
       <ContactForm />
     </div>
   );
