781312b0daf72cc75e60f5ec0e41fec8993001f9
Gagun45 <selyanchyn45@gmail.com>
2025-11-12 11:56:20 +0200

    contacts left card messages added

--- components/Main/Contacts/LeftCard/LeftCard.tsx
+++ components/Main/Contacts/LeftCard/LeftCard.tsx
@@ -1,23 +1,24 @@
 import { motion } from "framer-motion";
 import ContactsContainer from "./ContactsContainer/ContactsContainer";
+import { useTranslations } from "next-intl";
 
 const LeftCard = () => {
+  const t = useTranslations("Contacts.LeftCard");
+  const title = t("title");
+  const subtitle = t("subtitle");
   return (
     <motion.div
-          initial={{ opacity: 0, x: -40 }}
-          whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.6 }}
-          viewport={{ once: true }}
-          className="flex flex-col justify-center"
-        >
-          <h2>Get a Free Quote</h2>
-          <p className="sectionSubtitle">
-            Need a repair or want to know the price? Fill out the form and we’ll
-            get back to you shortly.
-          </p>
+      initial={{ opacity: 0, x: -40 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.6 }}
+      viewport={{ once: true }}
+      className="flex flex-col justify-center"
+    >
+      <h2>{title}</h2>
+      <p className="sectionSubtitle">{subtitle}</p>
 
-          <ContactsContainer />
-        </motion.div>
-  )
-}
-export default LeftCard
+      <ContactsContainer />
+    </motion.div>
+  );
+};
+export default LeftCard;
