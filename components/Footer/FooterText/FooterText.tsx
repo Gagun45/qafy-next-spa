c1c64dc79680826b7869403a00217e352fde9235
Gagun45 <selyanchyn45@gmail.com>
2025-11-13 10:21:54 +0200

    footertext messages added

--- components/Footer/FooterText/FooterText.tsx
+++ components/Footer/FooterText/FooterText.tsx
@@ -1,11 +1,12 @@
+import { useTranslations } from "next-intl";
+
 const FooterText = () => {
+  const t = useTranslations("Footer.FooterText");
+  const desc = t("desc");
   return (
     <div className="h-full">
       <h3 className="text-2xl font-bold mb-3">Qafy Mobile</h3>
-      <p>
-        Fast and reliable repair service for phones, PCs, and laptops. Your
-        tech, restored to life ⚡
-      </p>
+      <p>{desc}</p>
     </div>
   );
 };
