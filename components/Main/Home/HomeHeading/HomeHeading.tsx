ced8df95da1e08a5687c3ff23a4ff99ab6a67233
Gagun45 <selyanchyn45@gmail.com>
2025-11-14 16:03:00 +0200

    motion removed from hero img

--- components/Main/Home/HomeHeading/HomeHeading.tsx
+++ components/Main/Home/HomeHeading/HomeHeading.tsx
@@ -1,21 +1,13 @@
-import { motion } from "framer-motion";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
-
-const MotionImage = motion(Image);
 
 const HomeHeading = () => {
   const t = useTranslations("Home.HomeHeading");
   return (
     <>
-      <MotionImage
+      <Image
         src="/pngegg_optimized.webp"
-        width={250}
-        height={250}
         alt="Qafy Mobile Logo"
-        initial={{ scale: 0.8, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1 }}
-        transition={{ delay: 0.3, duration: 0.6 }}
         className="mb-6 mx-auto"
         priority
         sizes="250px"
