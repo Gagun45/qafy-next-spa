44041276c3c7304b033e03b22e12584634709091
Gagun45 <selyanchyn45@gmail.com>
2025-11-14 15:59:02 +0200

    hero image from png to webp

--- components/Main/Home/HomeHeading/HomeHeading.tsx
+++ components/Main/Home/HomeHeading/HomeHeading.tsx
@@ -2,29 +2,33 @@ import { motion } from "framer-motion";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 
+const MotionImage = motion(Image);
+
 const HomeHeading = () => {
-  const t = useTranslations('Home.HomeHeading')
+  const t = useTranslations("Home.HomeHeading");
   return (
     <>
-      <motion.div
+      <MotionImage
+        src="/pngegg_optimized.webp"
+        width={250}
+        height={250}
+        alt="Qafy Mobile Logo"
         initial={{ scale: 0.8, opacity: 0 }}
         animate={{ scale: 1, opacity: 1 }}
         transition={{ delay: 0.3, duration: 0.6 }}
-        className="flex justify-center mb-6"
-      >
-        <Image src={'/pngegg.png'} width={250} height={250} alt="home"/>
-      </motion.div>
+        className="mb-6 mx-auto"
+        priority
+        sizes="250px"
+      />
 
       {/* Title */}
       <h1 className="text-4xl sm:text-6xl text-center font-bold leading-tight mb-4">
-        {t('titleFirst')} <br />
-        <span className="text-primary">{t('titleSecond')}</span>
+        {t("titleFirst")} <br />
+        <span className="text-primary">{t("titleSecond")}</span>
       </h1>
 
       {/* Subtitle */}
-      <p className="text-lg sm:text-xl mb-8 text-center">
-        {t('subtitle')}
-      </p>
+      <p className="text-lg sm:text-xl mb-8 text-center">{t("subtitle")}</p>
     </>
   );
 };
