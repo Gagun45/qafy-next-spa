113fc3b3ae4bf4bc32de4c7c0a026809b1ff2885
Gagun45 <selyanchyn45@gmail.com>
2025-12-09 18:03:48 +0200

    desktop nav ul added, SmoothLink.tsx duration fixed

--- components/Header/DesktopNav/DesktopNav.tsx
+++ components/Header/DesktopNav/DesktopNav.tsx
@@ -1,17 +1,21 @@
 import SmoothLink from "@/components/General/SmoothLink/SmoothLink";
-import { LINKS } from "@/lib/constants";
-import { useTranslations } from "next-intl";
+import {LINKS} from "@/lib/constants";
+import {useTranslations} from "next-intl";
 
 const DesktopNav = () => {
-  const t = useTranslations("NavLinks");
-  return (
-    <nav className="items-center gap-8 xl:gap-12 hidden lg:flex">
-      {LINKS.map((link) => (
-        <SmoothLink key={link.href} to={link.href} spy={true}>
-          {t(`${link.href}`)}
-        </SmoothLink>
-      ))}
-    </nav>
-  );
+    const t = useTranslations("NavLinks");
+    return (
+        <nav>
+            <ul className="items-center gap-8 xl:gap-12 hidden lg:flex">
+                {LINKS.map((link) => (
+                    <li key={link.href}>
+                        <SmoothLink  to={link.href} spy={true}>
+                            {t(`${link.href}`)}
+                        </SmoothLink>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
 };
 export default DesktopNav;
